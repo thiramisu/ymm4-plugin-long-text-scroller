0fdd1e1201c8a86030b69fec5afb908bd0fd2f90
tiramisu_oTATo <xxx.partyo@gmail.com>
2025-10-24 04:17:14 +0900

    Refactor: ファイル移動のためほぼコピペ

--- LongTextScrollerPlugin/Utils/EnumExtensions.cs
+++ LongTextScrollerPlugin/Utils/EnumExtensions.cs
@@ -1,4 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using YukkuriMovieMaker.Plugin;
+using YukkuriMovieMaker.Settings;
 
 namespace LongTextScrollerPlugin.Utils;
 
@@ -11,5 +16,44 @@ public static class EnumExtensions
                          .Cast<DisplayAttribute>()
                          .FirstOrDefault();
         return attr?.ShortName ?? attr?.Name ?? value.ToString();
+    }
+    public static object Convert(Enum enumValue)
+    {
+        var memberInfo = enumValue.GetType().GetMember(enumValue.ToString());
+        if (memberInfo.Length > 0)
+        {
+            var displayAttr = memberInfo[0].GetCustomAttribute<DisplayAttribute>();
+            if (displayAttr != null)
+            {
+                // ResourceType が指定されている場合はリソースから取得
+                if (displayAttr.ResourceType != null)
+                {
+                    var resourceManager = new ResourceManager(displayAttr.ResourceType);
+                    var resourceName = displayAttr.Name;
+                    var culture = SettingsBase<YMMSettings>.Default.Language;
+
+                    if (!string.IsNullOrEmpty(resourceName))
+                    {
+                        var resourceValue = culture == Language.Default
+                            ? resourceManager.GetString(resourceName)
+                            : resourceManager.GetString(resourceName, new CultureInfo((int)culture));
+                        if (!string.IsNullOrEmpty(resourceValue))
+                        {
+                            return resourceValue;
+                        }
+                    }
+                }
+
+                // ResourceType がない場合、Name プロパティを直接使う
+                if (!string.IsNullOrEmpty(displayAttr.Name))
+                {
+                    return enumValue.GetShortDisplayName();
+                }
+            }
+        }
+
+        // Display 属性が見つからない場合は enum の名前を返す
+        return enumValue.ToString();
+
     }
 }
