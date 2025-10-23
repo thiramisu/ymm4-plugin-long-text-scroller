594786769379cc96cff8c58ec13f42c71fdf8d9a
tiramisu_oTATo <xxx.partyo@gmail.com>
2025-10-24 04:12:15 +0900

    Refactor: Converterの実際の処理をstaticに移動

--- LongTextScrollerPlugin/Utils/EnumDescriptionConverter.cs
+++ LongTextScrollerPlugin/Utils/EnumDescriptionConverter.cs
@@ -11,51 +11,48 @@ namespace LongTextScrollerPlugin.Utils;
 public class EnumDescriptionConverter : IValueConverter
 {
     public static EnumDescriptionConverter Instance { get; } = new();
-    public static object Convert(Enum Enum) => Instance.Convert(Enum, null, null, null);
-
-    public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? _)
+    public static object Convert(Enum enumValue)
     {
-        if (value is Enum enumValue)
+        var memberInfo = enumValue.GetType().GetMember(enumValue.ToString());
+        if (memberInfo.Length > 0)
         {
-            var memberInfo = enumValue.GetType().GetMember(enumValue.ToString());
-            if (memberInfo.Length > 0)
+            var displayAttr = memberInfo[0].GetCustomAttribute<DisplayAttribute>();
+            if (displayAttr != null)
             {
-                var displayAttr = memberInfo[0].GetCustomAttribute<DisplayAttribute>();
-                if (displayAttr != null)
+                // ResourceType が指定されている場合はリソースから取得
+                if (displayAttr.ResourceType != null)
                 {
-                    // ResourceType が指定されている場合はリソースから取得
-                    if (displayAttr.ResourceType != null)
-                    {
-                        var resourceManager = new ResourceManager(displayAttr.ResourceType);
-                        var resourceName = displayAttr.Name;
-                        var culture = SettingsBase<YMMSettings>.Default.Language;
+                    var resourceManager = new ResourceManager(displayAttr.ResourceType);
+                    var resourceName = displayAttr.Name;
+                    var culture = SettingsBase<YMMSettings>.Default.Language;
 
-                        if (!string.IsNullOrEmpty(resourceName))
+                    if (!string.IsNullOrEmpty(resourceName))
+                    {
+                        var resourceValue = culture == Language.Default
+                            ? resourceManager.GetString(resourceName)
+                            : resourceManager.GetString(resourceName, new CultureInfo((int)culture));
+                        if (!string.IsNullOrEmpty(resourceValue))
                         {
-                            var resourceValue = culture == Language.Default
-                                ? resourceManager.GetString(resourceName)
-                                : resourceManager.GetString(resourceName, new CultureInfo((int)culture));
-                            if (!string.IsNullOrEmpty(resourceValue))
-                            {
-                                return resourceValue;
-                            }
+                            return resourceValue;
                         }
                     }
+                }
 
-                    // ResourceType がない場合、Name プロパティを直接使う
-                    if (!string.IsNullOrEmpty(displayAttr.Name))
-                    {
-                        return enumValue.GetShortDisplayName();
-                    }
+                // ResourceType がない場合、Name プロパティを直接使う
+                if (!string.IsNullOrEmpty(displayAttr.Name))
+                {
+                    return enumValue.GetShortDisplayName();
                 }
             }
-
-            // Display 属性が見つからない場合は enum の名前を返す
-            return enumValue.ToString();
         }
 
-        return value?.ToString() ?? string.Empty;
+        // Display 属性が見つからない場合は enum の名前を返す
+        return enumValue.ToString();
+
     }
+
+    public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? _) => (value is Enum enumValue) ? Convert(enumValue) : value?.ToString() ?? string.Empty;
+
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 }
